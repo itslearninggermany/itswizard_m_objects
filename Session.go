e7ab1f6ffdbf830f94f1dda094fbcc39abd97a2b
nordmannconsulting <stdvwx38A!>
2023-09-27 21:55:00 +0200

    Initial commit

---
+++ Session.go
@@ -0,0 +1,13 @@
+package itswizard_m_objects
+
+import "github.com/jinzhu/gorm"
+
+/*
+In this struct the SessionID and IPAdress ist stored also the create, delete and update time
+*/
+type DbSession15 struct {
+	gorm.Model
+	UserID    uint   `gorm:"not null"`
+	SessionId string `gorm:"not null;type:VARCHAR(50)"`
+	IPAddress string `gorm:"not null; type:VARCHAR(500)"`
+}
---
+++ SessionUser.go
@@ -0,0 +1,31 @@
+package itswizard_m_objects
+
+/*
+The Session User is the data which is stored in the cookie.
+It must be called gob.Register (itswizard_objects.SessionUser{})
+*/
+type SessionUser struct {
+	Username            string
+	UserID              uint
+	FirstAuthentication bool
+	Authenticated       bool
+	TwoFac              bool
+	Firstname           string
+	Lastname            string
+	Mobile              string
+	IpAddress           string
+	Institution         string
+	School              string
+	Email               string
+	Information         string
+	Admin               bool
+	OrganisationID      uint
+	InstitutionID       uint
+}
+
+/*
+Creates a pointer to a SessionUser
+*/
+func NewSessionUser() *SessionUser {
+	return new(SessionUser)
+}
